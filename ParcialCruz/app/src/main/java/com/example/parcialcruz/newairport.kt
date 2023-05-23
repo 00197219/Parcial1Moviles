f0aafcfd1aa60b21e8431d7a09515eddd248c088
00197219 <00197219@uca.edu.sv>
2023-05-23 10:40:28 -0600

    ...

--- ParcialCruz/app/src/main/java/com/example/parcialcruz/newairport.kt
+++ ParcialCruz/app/src/main/java/com/example/parcialcruz/newairport.kt
@@ -1,59 +1,70 @@
 package com.example.parcialcruz
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.parcialcruz.R
+import com.example.parcialcruz.R
+import com.example.parcialcruz.databinding.FragmentNewairportBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class NewMovieFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [newairport.newInstance] factory method to
- * create an instance of this fragment.
- */
-class newairport : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel: airportViewModel by activityViewModels{
+        airportViewModel.Factory
     }
+
+
+
+    private lateinit var binding: FragmentNewairportBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_newairport, container, false)
+        binding = FragmentNewairportBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment newairport.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            newairport().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+        setViewModel()
+        observeStatus()
+    }
+
+
+    private fun setViewModel(){
+        binding.viewmodel = viewModel
+    }
+
+    private fun observeStatus () {
+        viewModel.status.observe(viewLifecycleOwner){ status ->
+            when{
+                status.equals(MovieViewModel.MOVIE_CTREATTED) ->{
+                    Log.d("APP TAG", status)
+                    Log.d("APP TAG", viewModel.getMovies().toString())
+
+                    viewModel.clearStatus()
+                    viewModel.clearData()
+
+                    findNavController().popBackStack()
+                }
+                status.equals(MovieViewModel.WRONG_DATA) ->{
+                    Log.d("APP TAG", status)
+                    viewModel.clearStatus()
+
                 }
             }
+        }
     }
+
+
 }
